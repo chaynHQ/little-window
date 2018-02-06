2b689cdf9de8be364496d25912112dc52d51d941
rb50 <rebecca-holmes@live.co.uk>
2018-02-06 15:38:34 +0000

    googlesheets working

--- src/userMessage.js
+++ src/userMessage.js
@@ -1,29 +1,43 @@
-const { DF_KEY } = require("../config");
+const { DF_KEY, GOOGLE_API_1, GOOGLE_API_2 } = require("../config");
 const apiai = require("apiai");
+const request = require("request");
 
 const app = apiai(DF_KEY);
 
 const userMessage = (req, res) => {
   const { question } = req.body;
 
-  const request = app.textRequest(question, {
+  const requestdf = app.textRequest(question, {
     sessionId: "1"
   });
 
-  request.on("response", function(response) {
-    console.log(response.result.fulfillment.messages);
+  requestdf.on("response", function(response) {
+    const lookup = {
+      DivorceIndia: "A:A",
+      DivorcePakistan: "B:B",
+      DivorceItaly: "C:C",
+      DivorceUK: "D:D"
+    };
 
     const { messages } = response.result.fulfillment;
 
-    res.send(messages);
+    const resources = messages[1] ? messages[1].payload : null;
+    if (resources) {
+      const cellRef = lookup[resources];
+      const url = GOOGLE_API_1 + cellRef + GOOGLE_API_2;
+      request(url, (err, res, body) => {
+        console.log(body);
+      });
+    }
 
+    res.send(messages);
   });
 
-  request.on("error", function(error) {
+  requestdf.on("error", function(error) {
     console.log(error);
   });
 
-  request.end();
+  requestdf.end();
 };
 
 module.exports = userMessage;
