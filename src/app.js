0221adbe1a6309ebd411999fbc92f2781219aec0
Jamie Coe <jamiecoe66@gmail.com>
2018-02-06 11:59:01 +0000

    Connected FE and BE relates to #15

---
+++ src/app.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+// const routes = require('./controllers/routes');
+const userMessage = require('./userMessage');
+
+const bodyParser = require('body-parser');
+
+app.set('port', process.env.PORT || 3001);
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
+
+// app.use(routes);
+
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, '..', 'client', 'build')));
+}
+
+app.post('/usermessage', userMessage)
+
+module.exports = app;
