612c4e9c76c0ec63b476a94c3ae4c47ad39e9cf0
Natalie <njseeto@users.noreply.github.com>
2018-02-08 15:08:27 +0000

    retrigger function working

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,28 @@
 import React, { Component } from "react";
 
 class App extends Component {
+  sendMessage(data) {
+    this.sendToServer(data)
+      .then(res => res.json())
+      .then(resData => {
+        if (resData.retrigger) {
+          console.log(resData)
+          this.sendMessage({
+            question: resData.retrigger
+          })
+        } else console.log(resData);
+      })
+  }
+
+  sendToServer(data) {
+    return fetch("/usermessage", {
+      method: "POST",
+      headers: new Headers({ "Content-Type": "application/json" }),
+      credentials: "same-origin",
+      body: JSON.stringify(data)
+    })
+  }
+
   handleSubmit(e) {
     e.preventDefault();
 
@@ -9,21 +31,13 @@ class App extends Component {
     for (const pair of new FormData(e.target).entries()) {
       data[pair[0]] = pair[1];
     }
-
-    fetch("/usermessage", {
-      method: "POST",
-      headers: new Headers({ "Content-Type": "application/json" }),
-      credentials: "same-origin",
-      body: JSON.stringify(data)
-    })
-      .then(res => res.json())
-      .then(data => console.log(data));
+    this.sendMessage(data);
   }
 
   render() {
     return (
       <div>
-        <form onSubmit={this.handleSubmit}>
+        <form onSubmit={this.handleSubmit.bind(this)}>
           <input type="text" name="question" />
           <input type="submit" value="Submit" />
         </form>
