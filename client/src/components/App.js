4dc8a1b74053ccc8e986d50526fc7b6c249c50d7
Jamie Coe <jamiecoe66@gmail.com>
2018-02-12 17:26:28 +0000

    refresh button working and saving chatlog to DB

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -20,51 +20,76 @@ export default class App extends React.Component {
     });
   };
 
-    sendMessage(data) {
-      this.sendToServer(data)
-        .then(res => res.json())
-        .then(resData => {
-          if (resData.retrigger) {
-            console.log(resData);
-            this.sendMessage({
-              speech: resData.retrigger
-            });
-          }
-        
-          if (resData.options.length === 0) {
-            this.setState({inputStatus: false})
-          } else {
-            this.setState({inputStatus: true})
-          }
+  componentDidMount() {
+    this.sendMessage({
+      speech: "Little window welcome"
+    });
 
-          resData.isUser = false;
-          resData.isWaiting = false;
-       
-          this.addMessage(resData);
-        });
-    }
+    window.addEventListener("onbeforeunload", () => {
+      alert('Closing tab!');
+    });
+  }
 
-    sendToServer(data) {
-      return fetch("/usermessage", {
-        method: "POST",
-        headers: new Headers({ "Content-Type": "application/json" }),
-        credentials: "same-origin",
-        body: JSON.stringify(data)
+  componentWillUnmount() {
+    // window.removeEventListener('beforeunload', this.saveChatLog);
+  }
+
+  sendMessage(data) {
+    this.sendToServer(data)
+      .then(res => res.json())
+      .then(resData => {
+        if (resData.retrigger) this.sendMessage({speech: resData.retrigger});
+
+        if (resData.options.length === 0) {
+          this.setState({inputStatus: false})
+        } else {
+          this.setState({inputStatus: true})
+        }
+
+        resData.isUser = false;
+        resData.isWaiting = false;
+
+        this.addMessage(resData);
       });
+  }
+
+  sendToServer(data) {
+    return fetch("/usermessage", {
+      method: "POST",
+      headers: new Headers({ "Content-Type": "application/json" }),
+      credentials: "same-origin",
+      body: JSON.stringify(data)
+    });
+  }
+
+  saveChatLog() {
+    console.log('Saving Chatlog!');
+    const data = {
+      messages: this.state.messages
     }
+
+    fetch("/savechatlog", {
+       method: "POST",
+       headers: new Headers({ "Content-Type": "application/json" }),
+       credentials: "same-origin",
+       body: JSON.stringify(data)
+     });
+  }
 
   refresh = () => {
+    this.saveChatLog();
     this.setState({
-      messages: [
-
-      ]
-    })
+      messages: []
+    });
+    this.sendMessage({
+      speech: "Little window welcome"
+    });
   };
 
   render() {
     return (
       <div>
-        <Header />
+        <Header refresh={this.refresh.bind(this)}/>
         <Conversation messages={this.state.messages} addMessage={this.addMessage.bind(this)} sendMessage={this.sendMessage.bind(this)}/>
         <Input addMessage={this.addMessage.bind(this)} sendMessage={this.sendMessage.bind(this)} inputStatus={this.state.inputStatus} />
       </div>
