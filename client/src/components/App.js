734de90b3592793846ae593bf1a601b8d3eab7fb
Johanna-hub <jyherman@gmail.com>
2018-02-12 17:17:57 +0000

    timeout functioning for chat reply

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -12,61 +12,87 @@ export default class App extends React.Component {
     };
   }
 
-  addMessage = message => {
+  addMessage = (message, replaceDots) => {
+    if (message.isUser === false) {
+      setTimeout(() => {
+        this.setState((prevState, props) => {
+          return {
+            messages: [...prevState.messages, message]
+          };
+        }),
+          this.replaceDots;
+      }, 3000);
+    } else {
+      this.setState((prevState, props, replaceDots) => {
+        return {
+          messages: [...prevState.messages, message]
+        };
+      });
+    }
+  };
+
+  replaceDots = message => {
     this.setState((prevState, props) => {
-      return {
-        messages: [...prevState.messages, message]
-      };
+      if (prevState.messages[prevState.messages.length - 1].speech === "...") {
+        return {
+          message: [...prevState.messages.slice(0, -1), message]
+        };
+      }
     });
   };
 
-    sendMessage(data) {
-      this.sendToServer(data)
-        .then(res => res.json())
-        .then(resData => {
-          if (resData.retrigger) {
-            console.log(resData);
-            this.sendMessage({
-              speech: resData.retrigger
-            });
-          }
-        
-          if (resData.options.length === 0) {
-            this.setState({inputStatus: false})
-          } else {
-            this.setState({inputStatus: true})
-          }
+  sendMessage(data) {
+    this.sendToServer(data)
+      .then(res => res.json())
+      .then(resData => {
+        if (resData.retrigger) {
+          this.sendMessage({
+            speech: resData.retrigger
+          });
+        }
 
-          resData.isUser = false;
-          resData.isWaiting = false;
-       
-          this.addMessage(resData);
-        });
-    }
+        if (resData.options.length === 0) {
+          this.setState({ inputStatus: false });
+        } else {
+          this.setState({ inputStatus: true });
+        }
 
-    sendToServer(data) {
-      return fetch("/usermessage", {
-        method: "POST",
-        headers: new Headers({ "Content-Type": "application/json" }),
-        credentials: "same-origin",
-        body: JSON.stringify(data)
+        resData.isUser = false;
+        resData.isWaiting = false;
+
+        this.addMessage(resData);
       });
-    }
+  }
+
+  sendToServer(data) {
+    return fetch("/usermessage", {
+      method: "POST",
+      headers: new Headers({ "Content-Type": "application/json" }),
+      credentials: "same-origin",
+      body: JSON.stringify(data)
+    });
+  }
 
   refresh = () => {
     this.setState({
-      messages: [
-
-      ]
-    })
+      messages: []
+    });
   };
 
   render() {
     return (
       <div>
         <Header />
-        <Conversation messages={this.state.messages} addMessage={this.addMessage.bind(this)} sendMessage={this.sendMessage.bind(this)}/>
-        <Input addMessage={this.addMessage.bind(this)} sendMessage={this.sendMessage.bind(this)} inputStatus={this.state.inputStatus} />
+        <Conversation
+          messages={this.state.messages}
+          addMessage={this.addMessage.bind(this)}
+          sendMessage={this.sendMessage.bind(this)}
+        />
+        <Input
+          addMessage={this.addMessage.bind(this)}
+          sendMessage={this.sendMessage.bind(this)}
+          inputStatus={this.state.inputStatus}
+        />
       </div>
     );
   }
--- client/src/components/buttons/index.js
+++ client/src/components/buttons/index.js
@@ -6,26 +6,40 @@ export default class Button extends React.Component {
     super(props);
   }
 
-  clickHandler(speech, postback){
+  clickHandler(speech, postback) {
     const data = {
       isUser: true,
       isWaiting: true,
       speech
-    }
+    };
+
+    const buttonDots = {
+      speech: "..."
+    };
+
     console.log("postback", postback);
     this.props.addMessage(data);
-    this.props.sendMessage({speech: postback});
+    this.props.addMessage(buttonDots);
+    this.props.sendMessage({ speech: postback });
   }
 
   render() {
-    if(!this.props.options){
+    if (!this.props.options) {
       return null;
     }
     return (
       <div>
-      {this.props.options.map((option, index) => {
-        return <button value={option.postback} key={index} onClick={()=> this.clickHandler(option.text, option.postback)}>
-        {option.text}</button>})}
+        {this.props.options.map((option, index) => {
+          return (
+            <button
+              value={option.postback}
+              key={index}
+              onClick={() => this.clickHandler(option.text, option.postback)}
+            >
+              {option.text}
+            </button>
+          );
+        })}
       </div>
     );
   }
