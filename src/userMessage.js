e4b0295086c86372343360b464d48978f5d4bbf4
Jamie Coe <jamiecoe66@gmail.com>
2018-03-13 15:39:03 +0000

    remove unecessary functions

--- src/userMessage.js
+++ src/userMessage.js
@@ -7,23 +7,6 @@ const gSheetLookup = require('./googleSheetRef');
 
 const app = apiai(DF_KEY);
 
-// const getAndSendGSheetResources = url =>
-//   request(url)
-//     .then((body) => {
-//       const resourceArray = JSON.parse(body).values.map((resource) => {
-//         const singleResourceArray = resource.map((str, index, array) => {
-//           if (index % 2 === 1) return null;
-//           return { text: array[index], href: array[index + 1] };
-//         });
-//
-//         return singleResourceArray.filter(Boolean);
-//       });
-//       return [].concat(...resourceArray);
-//     })
-//     .catch((err) => {
-//       console.log(err);
-//     });
-
 const getResource = (countryObj) => {
   const cellRef = gSheetLookup[countryObj.lookup];
   const url = GOOGLE_API_1 + cellRef + GOOGLE_API_2;
@@ -43,13 +26,6 @@ const getResource = (countryObj) => {
     .catch((err) => {
       console.log(err);
     });
-};
-
-const findCellRef = (selectedCountries) => {
-  const { start } = gSheetLookup[selectedCountries[0].text];
-  const { end } = gSheetLookup[selectedCountries[selectedCountries.length - 1].text];
-
-  return `${start}:${end}`;
 };
 
 const apiaiCall = (req, res, speech) => {
