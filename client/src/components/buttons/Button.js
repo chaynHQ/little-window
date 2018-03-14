3669fd2eb83739b8d865732558cf325a8add21c1
Johanna-hub <jyherman@gmail.com>
2018-03-14 17:08:38 +0000

    button component closes #90

--- client/src/components/buttons/Button.js
+++ client/src/components/buttons/Button.js
@@ -22,7 +22,7 @@ const Styledbutton = styled.button`
   cursor: pointer;
 `;
 
-const Countrybutton = styled.button`
+const Multiplebutton = styled.button`
   border: 2px #b0b0b0 solid;
   color: black;
   background: white;
@@ -64,33 +64,20 @@ export default class Button extends Component {
   render() {
     if (!this.props.options || this.state.disabled) return null;
 
-    if (this.props.options.length > 2) {
-      return (
-        <Botbuttons>
-          {this.props.options.map(option => (
-            <Countrybutton
-              value={option.postback}
-              key={option.postback}
-              onClick={() => this.clickHandler(option.text, option.postback)}
-            >
-              {option.text}
-            </Countrybutton>
-          ))}
-        </Botbuttons>
-      );
-    }
+    const ButtonName =
+          this.props.options.length > 2 ? Multiplebutton : Styledbutton;
     return (
       <Botbuttons>
-        {this.props.options.map(option => (
-          <Styledbutton
+        {this.props.options.map((option, index) => (
+          <ButtonName
             value={option.postback}
-            key={option.postback}
+            key={index}
             onClick={() => this.clickHandler(option.text, option.postback)}
           >
             {option.text}
-          </Styledbutton>
-        ))}
+          </ButtonName>
+            ))}
       </Botbuttons>
-    );
+    )
   }
 }
