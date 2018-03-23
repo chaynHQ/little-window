0a626b5a0f72e1803160e4bd8ba6892ef6b6b872
Johanna-hub <jyherman@gmail.com>
2018-03-23 13:05:29 +0000

    snapshot test fix

--- client/src/components/header/Header.test.js
+++ client/src/components/header/Header.test.js
@@ -5,10 +5,14 @@ import Header from './Header';
 
 const refresh = () => { };
 const refreshDisabled = false;
+const minimise = true;
+const minimiseFunc = () => {};
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
   ReactDOM.render(<Header
+    minimise={minimise}
+    minimiseFunc={minimiseFunc}
     refresh={refresh}
     refreshDisabled={refreshDisabled}
   />, div);
