d0c837999f0fc4980284bef2cdc1d3fbce9565be
matthewdking <mattdking1@gmail.com>
2018-04-16 16:05:50 +0100

    #182 updates savemessage query

--- src/database/queries/save_message.js
+++ src/database/queries/save_message.js
@@ -1,27 +1,19 @@
 const db = require('../db_connections');
 const { databaseEmail } = require('../../emails');
 
-const saveMessage = (speech, conversationId) => db.query(
-//   'INSERT INTO messages (chat, conversation_id) VALUES ($1, (SELECT id FROM conversation WHERE uniqueId = $2))',
-//   [speech, conversationId],
-// )
-  `
-  IF ((SELECT * FROM conversation WHERE uniqueid = '$2') >= 1)
+// query only inserts into messages if the conversation id has been set
+// this is to stop the three automatic messages at the start being inputted into the db
+const saveMessage = (speech, conversationId) => db.query(`
+  DO $$
+  DECLARE conv_id integer := (SELECT id FROM conversation WHERE uniqueid = $2);
   BEGIN
+  IF (conv_id >= 0) THEN
   INSERT INTO messages (chat, conversation_id)
-  VALUES ($1, (SELECT id FROM conversation WHERE uniqueId = $2))
-  END IF`, [speech, conversationId])
+  VALUES ($1, (SELECT id FROM conversation WHERE uniqueId = $2));
+  END IF;
+  END $$;`, [speech, conversationId])
   .catch((error) => {
-    console.log(error);
-    console.log('db error fuck');
     databaseEmail('saving messages', error);
   });
 
 module.exports = saveMessage;
-
-
-IF ((SELECT * FROM conversation WHERE uniqueid = 'a1c09096-2c13-45ae-97fa-cea86a857c2b') >= 1)
-  BEGIN
-  INSERT INTO messages (chat, conversation_id)
-  VALUES ('blah', (SELECT id FROM conversation WHERE uniqueId = 'a1c09096-2c13-45ae-97fa-cea86a857c2b'))
-  END IF
