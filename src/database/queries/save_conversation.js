9416cbaf8ea9d0cb38a58242d3eb9409b5937a8d
Lucy King <lucy.m.king92@gmail.com>
2018-04-18 10:51:28 +0100

    remove console log

--- src/database/queries/save_conversation.js
+++ src/database/queries/save_conversation.js
@@ -2,7 +2,6 @@ const db = require('../db_connections');
 const { databaseEmail } = require('../../emails');
 
 const saveConversation = conversationId => {
-  console.log('inSaveConvo');
   db
     .query('INSERT INTO conversation (uniqueId) VALUES ($1)', [conversationId])
     .catch(error => {
