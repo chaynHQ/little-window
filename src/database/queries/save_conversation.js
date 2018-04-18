7dcfcacc5d73cbd105f2678cc7d8ce9045a820bc
Lucy King <lucy.m.king92@gmail.com>
2018-04-18 12:40:13 +0100

    formatting stuff

--- src/database/queries/save_conversation.js
+++ src/database/queries/save_conversation.js
@@ -2,10 +2,12 @@ const db = require('../db_connections');
 const { databaseEmail } = require('../../emails');
 
 const saveConversation = conversationId => {
-  db
+  return db
     .query('INSERT INTO conversation (uniqueId) VALUES ($1)', [conversationId])
     .catch(error => {
       databaseEmail('saving conversation', error);
     });
 };
+
 module.exports = saveConversation;
+
