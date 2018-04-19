89889add175893903253c5668ab5bca575270171
Lucy King <lucy.m.king92@gmail.com>
2018-04-19 15:49:07 +0100

    remove console.logs

--- src/googleCall.js
+++ src/googleCall.js
@@ -4,12 +4,8 @@ const request = require('request-promise');
 
 // returning resources from Google Sheets
 const getResource = (countryObj, lang) => {
-  console.log('lang', lang);
-  console.log('countryObj', countryObj);
   const cellRef = gSheetLookup[countryObj.lookup];
-  console.log('cellRef', cellRef);
   const url = `${GOOGLE_API_1}${lang}!${cellRef}${GOOGLE_API_2}`;
-  console.log('url', url);
 
   return request(url).then(body =>
     JSON.parse(body).values.reduce((acc, cur) => [...acc, { text: cur[0], href: cur[1] }], []));
