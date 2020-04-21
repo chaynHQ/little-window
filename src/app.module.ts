ce9971a2e5c77059454d75c7725c33ee62a4947d
Sophie Hebdidge <sophie.hebdidge@gmail.com>
2020-04-21 15:43:13 +0100

    connected to typeorm

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,19 @@
-require('dotenv').config();
+import * as dotenv from 'dotenv';
+dotenv.config();
+
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule } from '@nestjs/config'
 import { UserMessageController } from './userMessage/userMessage.controller';
 import { ConversationController } from './conversation/conversation.controller';
 import { AppService } from './app.service';
+import { databaseConfig } from './services/database-connection';
+
 
 @Module({
-  imports: [],
+  imports: [ConfigModule.forRoot({isGlobal: true}),
+    TypeOrmModule.forRoot(databaseConfig)
+  ],
   controllers: [UserMessageController, ConversationController],
   providers: [AppService],
 })
---
+++ src/services/database-connection.ts
@@ -0,0 +1,15 @@
+import { TypeOrmModuleOptions } from '@nestjs/typeorm';
+const url = require('url');
+
+const params = url.parse(process.env.DATABASE_URL);
+const [username, password] = params.auth.split(':');
+
+export const databaseConfig:TypeOrmModuleOptions = {
+      "type": "postgres",
+      "host": params.hostname,
+      "port": params.port,
+      "username": username,
+      "password": password,
+      "database": params.pathname.split('/')[1],
+      "url": process.env.DATABASE_URL
+    };
