0abc52c4e255d4d5f593a2cc153f846f525d2b77
Sophie Hebdidge <sophie.hebdidge@gmail.com>
2020-06-01 15:15:43 +0100

    Update to migrations

--- src/migration/1591020823760-AddCityAndCountryColumnsToConversation.ts
+++ src/migration/1591020823760-AddCityAndCountryColumnsToConversation.ts
@@ -1,10 +1,9 @@
 import {MigrationInterface, QueryRunner} from "typeorm";
 
-export class AddCityAndCountryColumnToConversation1591009528989 implements MigrationInterface {
-    name = 'AddCityAndCountryColumnToConversation1591009528989'
+export class AddCityAndCountryColumnsToConversation1591020823760 implements MigrationInterface {
+    name = 'AddCityAndCountryColumnsToConversation1591020823760'
 
     public async up(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query(`ALTER TABLE "conversation" DROP COLUMN "location"`, undefined);
         await queryRunner.query(`ALTER TABLE "conversation" ADD "city" character varying`, undefined);
         await queryRunner.query(`ALTER TABLE "conversation" ADD "country" character varying`, undefined);
     }
@@ -12,7 +11,6 @@ export class AddCityAndCountryColumnToConversation1591009528989 implements Migra
     public async down(queryRunner: QueryRunner): Promise<void> {
         await queryRunner.query(`ALTER TABLE "conversation" DROP COLUMN "country"`, undefined);
         await queryRunner.query(`ALTER TABLE "conversation" DROP COLUMN "city"`, undefined);
-        await queryRunner.query(`ALTER TABLE "conversation" ADD "location" character varying`, undefined);
     }
 
 }
