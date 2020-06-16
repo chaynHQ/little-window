fafc5a4fb6955f07b07605a632bea1bae0180b57
sophieheb <sophie.hebdidge@gmail.com>
2020-06-16 13:10:23 +0100

    Add in rollbar for storyblok frontend call (#258)

--- client/src/storyblok.js
+++ client/src/storyblok.js
@@ -1,23 +1,39 @@
+import Rollbar from "rollbar";
 const StoryblokClient = require('storyblok-js-client');
+
+
+const RollbarInstance = new Rollbar({
+        accessToken: process.env.REACT_APP_ROLLBAR_TOKEN,
+        captureUncaught: true,
+        captureUnhandledRejections: true,
+        payload: {
+          environment: process.env.REACT_APP_ROLLBAR_ENV
+        },
+        enabled: ('%NODE_ENV%' === 'production')
+      })
 
 const Storyblok = new StoryblokClient({
   accessToken: process.env.REACT_APP_STORYBLOK_TOKEN,
 });
 
 async function getNewConversationMessage() {
-  const storyblokResponse = await Storyblok.get('cdn/stories/setup/language');
-  const storyblokResponseContent = storyblokResponse.data.story;
+  try {
+    const storyblokResponse = await Storyblok.get('cdn/stories/setup/language');
+    const storyblokResponseContent = storyblokResponse.data.story;
 
-  const message = [];
-  storyblokResponseContent.content.speech.items.forEach((item) => {
-    message.push(
-      { speech: item, storyblokId: storyblokResponseContent.uuid },
-    );
-  });
+    const message = [];
+    storyblokResponseContent.content.speech.items.forEach((item) => {
+      message.push(
+        { speech: item, storyblokId: storyblokResponseContent.uuid },
+      );
+    });
 
-  message[message.length - 1].checkBoxOptions = storyblokResponseContent.content.checkBoxOptions;
+    message[message.length - 1].checkBoxOptions = storyblokResponseContent.content.checkBoxOptions;
 
-  return message;
+    return message;
+  } catch (error){
+    throw new Error(error);
+  }
 }
 
 export default getNewConversationMessage;
